4ff2d448baf7553adfbbc0d305a26b26cf0f8e94
TatianaKapos <tatianakapos@microsoft.com>
2024-04-16 11:22:19 -0700

    upgrade to 74

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ['@babel/plugin-proposal-class-properties'],
+    ['@babel/plugin-proposal-private-methods'],
+    ['@babel/plugin-proposal-private-property-in-object'],
+  ],
 };
